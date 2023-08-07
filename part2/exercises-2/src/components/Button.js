a1962b897223a57b0f38402eb617d9291fb9919f
Suganthi <suganthisomalinga@gmail.com>
2023-08-07 09:40:33 -0500

    commit

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -1,9 +1,11 @@
 import './styles.css';
 
 function Button() {
-
+ function onLearnMore(){
+   alert("SplishSplash");
+ }
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
